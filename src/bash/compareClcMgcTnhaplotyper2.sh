13a57c92ed6d60209436b5a26789cb0d58c9e9f0
m006703 <sakai.yuta@mayo.edu>
2020-06-10 10:52:13 -0500

    finished all steps in script

--- src/bash/compareClcMgcTnhaplotyper2.sh
+++ src/bash/compareClcMgcTnhaplotyper2.sh
@@ -45,6 +45,25 @@ SCRIPT_NAME="$(basename ${0})"
 SCRIPT_ROOT="$(cd "${SCRIPT_DIR}/../../" && pwd)"
 PROFILE="${SCRIPT_ROOT}/config/compareVcf.profile"
 SAMPLEDIR=""
+COMMON_FUNC=""
+LOG_DIR=""
+LOG_FILE=""
+SAMPLE=""
+CLC_VCF=""
+BGZIP=""
+TABIX=""
+CMD=""
+MGC_VCF=""
+BCFTOOLS=""
+FILTERED_MGC_VCF=""
+PYTHON2710=""
+HAPPY_DIR=""
+HAPPY=""
+REF_GENOME=""
+HAPPY_ARGS=""
+FP_VCF=""
+FN_VCF=""
+HAPPY_VCF=""
 
 ##################################################
 #Source Pipeline Profile
@@ -85,14 +104,74 @@ set -o nounset
 #BEGIN PROCESSING
 ##################################################
 
-while getopts "hdi:s:" OPTION
+while getopts "hi:" OPTION
 
 do
   case $OPTION in
     h) echo "${DOCS}" ; rm ${LOG_FILE} ; exit ;;
-    d) DEBUG="-d" ; set -x ;;
     i) SAMPLEDIR="${OPTARG}" ;;
-    s) SAMPLE="${OPTARG}" ;;
     ?) echo "${DOCS}" ; rm ${LOG_FILE} ; exit ;;
   esac
 done
+
+# Check arguments
+validateParm "SAMPLEDIR" "Please specify the option: i"
+validateDirectory "${SAMPLEDIR}"
+
+# Remove any trailing "/" from SAMPLEDIR
+SAMPLEDIR=${SAMPLEDIR%/}
+
+# Get the sample name
+SAMPLE=$(basename ${SAMPLEDIR})
+logInfo "Processing sample: ${SAMPLE}"
+logInfo "in: ${SAMPLEDIR}"
+
+# Source python 2.7.10
+logInfo "Sourcing python 2.7.10"
+CMD="source ${PYTHON2710}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+
+# bgzip and tabix the original CLC VCF
+logInfo "bgzipping the original CLC VCF: ${SAMPLEDIR}/${SAMPLE}_cmb.vcf"
+CMD="${BGZIP} ${SAMPLEDIR}/${SAMPLE}_cmb.vcf"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+CLC_VCF="${SAMPLEDIR}/${SAMPLE}_cmb.vcf.gz"
+logInfo "tabix-ing the bgzipped CLC VCF: ${CLC_VCF}"
+CMD="${TABIX} -p vcf ${CLC_VCF}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+
+# Filter the "strand_artifact" variants from MGC VCF
+MGC_VCF="${SAMPLEDIR}/mgc/reports/${SAMPLE}.vcf"
+logInfo "Filtering strand_artifact variants from MGC VCF: ${MGC_VCF}"
+FILTERED_MGC_VCF="${SAMPLEDIR}/mgc/reports/${SAMPLE}_filtered.vcf"
+CMD="${BCFTOOLS} view -e 'FILTER=\"strand_artifact\"' ${MGC_VCF} > ${FILTERED_MGC_VCF}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+
+# Run hap.py
+HAPPY_DIR="${SAMPLEDIR}/hap.py_out"
+logInfo "Creating output directory for hap.py: ${HAPPY_DIR}"
+CMD="mkdir ${HAPPY_DIR}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+logInfo "Running hap.py to compare CLC and filtered MGC VCF"
+CMD="${HAPPY} ${CLC_VCF} ${FILTERED_MGC_VCF} ${HAPPY_ARGS}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+
+# Separate the false positives and false negatives in separate VCF files
+logInfo "Creating VCF files for false positives and false negatives"
+FP_VCF="${HAPPY_DIR}/${SAMPLE}_CLC_MGC_false_positives.vcf"
+FN_VCF="${HAPPY_DIR}/${SAMPLE}_CLC_MGC_false_negatives.vcf"
+HAPPY_VCF="${HAPPY_DIR}/${SAMPLE}_CLC_MGC.vcf.gz"
+CMD="${BCFTOOLS} view -i 'FORMAT/BD=\"FP\"' ${HAPPY_VCF} > ${FP_VCF}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+CMD="${BCFTOOLS} view -i 'FORMAT/BD=\"FN\"' ${HAPPY_VCF} > ${FN_VCF}"
+logInfo "Executing command: ${CMD}"
+eval ${CMD}
+
+logInfo "Script is done running"
